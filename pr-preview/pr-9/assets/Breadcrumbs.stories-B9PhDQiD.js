f94b85a6503fe8fa0416d1c6732362ac69ce79e9
asafvaron90 <asafvaron90@users.noreply.github.com>
2026-03-25 22:39:52 +0000

    Remove preview for PR 9 🛬

--- pr-preview/pr-9/assets/Breadcrumbs.stories-B9PhDQiD.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{w as m}from"./decorator-B7uZuD-S.js";import{B as s,L as o}from"./Link-eQoXcpQm.js";import{T as a}from"./Typography-Bs6KzVE2.js";import"./tokens-DUrEFsAw.js";import"./index-JhL3uwfD.js";import"./createSvgIcon-DWL3pMwK.js";import"./ButtonBase-BZeBNmBl.js";import"./useTimeout-C3DxH1iD.js";import"./useForkRef-C3n9QMso.js";import"./useEventCallback-BzpeKxgC.js";import"./isFocusVisible-B8k4qzLc.js";import"./useSlotProps-Dzo_xN1q.js";import"./mergeSlotProps-BOVX1M_k.js";import"./useTheme-DfQJHpRG.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";const D={title:"System/Library Components (MUI)/Breadcrumbs",component:s,decorators:[m],parameters:{layout:"centered"}},e={render:()=>r.jsxs(s,{children:[r.jsx(o,{underline:"hover",color:"inherit",href:"#",children:"Design System"}),r.jsx(o,{underline:"hover",color:"inherit",href:"#",children:"Tokens"}),r.jsx(a,{color:"text.primary",children:"Colors"})]})};var t,i,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: () => <Breadcrumbs>
-      <Link underline="hover" color="inherit" href="#">Design System</Link>
-      <Link underline="hover" color="inherit" href="#">Tokens</Link>
-      <Typography color="text.primary">Colors</Typography>
-    </Breadcrumbs>
-}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,D as default};
--- pr-preview/pr-9/assets/useTheme-DfQJHpRG.js
+++
@@ -1,1 +0,0 @@
-import"./index-JhL3uwfD.js";import{u as t,T as m,d as s}from"./decorator-B7uZuD-S.js";function r(){const e=t(s);return e[m]||e}export{r as u};
